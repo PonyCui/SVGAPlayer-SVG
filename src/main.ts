56d0714cdf65e4832e17427ac4740e56ab87e2b3
PonyCui <cuiminghui2@bigo.sg>
2019-12-05 21:38:17 +0800

    vector not pass.

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { Generator } from "./generator";
 import { writeFileSync } from "fs";
 
 (async () => {
-    const a = await Parser.load("/Users/saiakirahui/Documents/OpenSource/SVGAPlayer-SVG/samples/PinJump.svga")
+    const a = await Parser.load("/Users/saiakirahui/Documents/OpenSource/SVGAPlayer-SVG/samples/Walkthrough.svga")
     const b = new Generator(a)
     await b.process()
-    writeFileSync("/Users/saiakirahui/Documents/OpenSource/SVGAPlayer-SVG/samples/PinJump.svg", b.toString());
+    writeFileSync("/Users/saiakirahui/Documents/OpenSource/SVGAPlayer-SVG/samples/Walkthrough.svg", b.toString());
 })();
